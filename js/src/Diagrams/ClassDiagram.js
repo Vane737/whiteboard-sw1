ac5f76f1470fc2fb118bf4892830885e18f39f67
Vane737 <77525733+Vane737@users.noreply.github.com>
2024-01-23 10:21:09 -0400

    first commit

---
+++ js/src/Diagrams/ClassDiagram.js
@@ -0,0 +1,87 @@
+/**
+ * Adds the general palette to the sidebar.
+ */
+const addClassDiagramPalette = function (sb, expand) {
+
+  // Reusable cells
+  var field = new mxCell('+ field: type', new mxGeometry(0, 0, 100, 26), 'text;html=1;strokeColor=none;fillColor=none;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;whiteSpace=wrap;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;');
+  var attributeField = new mxCell('+ field: type', new mxGeometry(0, 0, 100, 26), 'text;html=1;strokeColor=none;fillColor=none;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;whiteSpace=wrap;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;componentName=attribute');
+  var methodField = new mxCell('+ method(type): type', new mxGeometry(0, 0, 100, 26), 'text;html=1;strokeColor=none;fillColor=none;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;whiteSpace=wrap;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;componentName=method');
+  var _controller = new mxCell('Controller', new mxGeometry(0, 0, 100, 26), 'text;html=1;strokeColor=none;fillColor=none;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;whiteSpace=wrap;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;componentName=method');
+
+
+  field.vertex = true;
+  attributeField.vertex = true;
+  methodField.vertex = true;
+  _controller.vertex = true;
+
+  var divider = new mxCell('', new mxGeometry(0, 0, 40, 8), 'line;html=1;strokeWidth=1;fillColor=none;align=left;verticalAlign=middle;spacingTop=-1;spacingLeft=3;spacingRight=3;rotatable=0;labelPosition=right;points=[];portConstraint=eastwest;');
+  divider.vertex = true;
+
+  // Default tags
+  var dt = 'uml static class ';
+
+  var fns = [
+
+    sb.createVertexTemplateEntry('shape=umlLifeline;perimeter=lifelinePerimeter;whiteSpace=wrap;html=1;container=1;collapsible=0;recursiveResize=0;fillColor=#FFD18C;outlineConnect=0;', 100, 300, 'objeto:', 'Objeto', null, null, 'uml sequence participant lifeline'),
+    sb.createVertexTemplateEntry('shape=umlLifeline;participant=umlActor;perimeter=lifelinePerimeter;fillColor=#FFD18C;whiteSpace=wrap;html=1;container=1;collapsible=0;recursiveResize=0;verticalAlign=top;spacingTop=36;outlineConnect=0;',
+				20, 300, '', 'Actor', null, null, 'uml sequence participant lifeline actor'),
+
+    sb.createVertexTemplateEntry('shape=umlFrame;whiteSpace=wrap;fillColor=#ffdca6;html=1;', 300, 200, 'frame', 'Frame', null, null, 'uml sequence frame'),
+		sb.createVertexTemplateEntry('shape=umlDestroy;whiteSpace=wrap;html=1;strokeWidth=3;', 40, 30, '', 'Fin de ejecución', null, null, 'uml sequence destruction destroy'),
+    
+    sb.createVertexTemplateEntry('text;html=1;align=center;', 100, 25,
+    '[condición]', 'Label', null, null, dt + 'label'),
+
+    sb.createVertexTemplateEntry(
+      'text;html=1;align=center;fontStyle=1;verticalAlign=middle;spacingLeft=3;spacingRight=3;strokeColor=none;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;',
+      80, 26, 'Title', 'Title', null, null, dt + 'title label'
+    ),
+
+    sb.addEntry('uml sequence invoke call delegation synchronous invocation activation', function()
+		{
+	    	var cell = new mxCell('', new mxGeometry(0, 0, 10, 80), 'html=1;points=[];perimeter=orthogonalPerimeter;');
+	    	cell.vertex = true;
+	    	
+			var edge1 = new mxCell('accion', new mxGeometry(0, 0, 0, 0), 'html=1;verticalAlign=bottom;endArrow=block;dashed=4;entryX=0;entryY=0;');
+			edge1.geometry.setTerminalPoint(new mxPoint(-70, 0), true);
+			edge1.geometry.relative = true;
+			edge1.edge = true;
+
+			cell.insertEdge(edge1, false);
+			
+			var edge2 = new mxCell('retorno', new mxGeometry(0, 0, 0, 0), 'html=1;verticalAlign=bottom;endArrow=open;dashed=1;endSize=8;exitX=0;exitY=0.95;');
+			edge2.geometry.setTerminalPoint(new mxPoint(-70, 76), false);
+			edge2.geometry.relative = true;
+			edge2.edge = true;
+			
+			cell.insertEdge(edge2, true);
+			
+			return sb.createVertexTemplateFromCells([cell, edge1, edge2], 10, 80, 'Synchronous Invocation');
+		}),
+
+    sb.createVertexTemplateEntry('shape=umlActor;verticalLabelPosition=bottom;verticalAlign=top;fillColor=#FFD18C;html=1;', 30, 60, 'Actor', 'Actor', false, null, 'uml actor', 30, 60, '', 'Actor', null, null, dt + 'title label'),
+
+    sb.createVertexTemplateEntry('html=1;points=[];perimeter=orthogonalPerimeter;', 10, 80, '', 'Activation', null, null, 'uml sequence activation'),
+
+    sb.addEntry('uml sequence return message', function()
+		{
+			var edge = new mxCell('retorno', new mxGeometry(0, 0, 0, 0), 'html=1;verticalAlign=bottom;endArrow=open;dashed=1;endSize=8;');
+			edge.geometry.setTerminalPoint(new mxPoint(80, 0), true);
+			edge.geometry.setTerminalPoint(new mxPoint(0, 0), false);
+			edge.geometry.relative = true;
+			edge.edge = true;
+			
+			return sb.createEdgeTemplateFromCells([edge], 80, 0, 'Return');
+		}),
+    
+    sb.createEdgeTemplateEntry('edgeStyle=orthogonalEdgeStyle;rounded=0;html=1;entryX=0.5;entryY=0;dashed=4;jettySize=auto;orthogonalLoop=1;endArrow=block;endFill=1', 160, 0, 'inicio', 'Inicio de flujo', null, 'uml generalization extend'),
+
+    sb.createEdgeTemplateEntry('endArrow=open;endSize=12;dashed=2;html=1;', 160, 0, 'acción', 'Acción', null, 'uml generalization extend'),
+  ];
+
+  sb.addPaletteFunctions('classDiagram', mxResources.get('classDiagram'), expand || false, fns);
+
+};
+
+module.exports = addClassDiagramPalette;
